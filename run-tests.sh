18b90f9d14acc6fb7d3d4daa92585f3b85af4a00
eikek <eike.kettner@posteo.de>
2021-07-13 23:24:06 +0200

    Use db dump for testing

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,7 @@ base=$(dirname "$(readlink -f "$0")")
 start_docker() {
     cd $base/ci
     docker-compose -f docker-compose.yml up -d
+    sleep 3
 }
 
 stop_docker() {
@@ -17,8 +18,6 @@ stop_docker() {
 trap "{ stop_docker ; }" EXIT
 
 start_docker
-sleep 5
 
-cargo test --test setup
 cargo test --test login
 cargo test --test integration
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,6 +1,37 @@
 mod common;
 use crate::common::{mk_cmd, Result};
 use assert_cmd::prelude::*;
+use dsc::types::BasicResult;
+use std::process::Command;
+
+#[test]
+fn dsc_help() -> Result<()> {
+    let mut cmd = Command::cargo_bin("dsc")?;
+    let assert = cmd.arg("--help").assert();
+    assert.success().stderr("");
+    Ok(())
+}
+
+#[test]
+fn remote_version() -> Result<()> {
+    let mut cmd = mk_cmd()?;
+    let assert = cmd.arg("version").assert();
+    assert.success().stderr("");
+    Ok(())
+}
+
+#[test]
+fn remote_register() -> Result<()> {
+    let mut cmd = mk_cmd()?;
+    let out = cmd
+        .arg("register")
+        .args(&["-c", "demo2", "-l", "demo2", "--password", "test"])
+        .output()?;
+
+    let out: BasicResult = serde_json::from_slice(out.stdout.as_slice())?;
+    assert!(out.success);
+    Ok(())
+}
 
 #[test]
 fn remote_upload_web() -> Result<()> {
