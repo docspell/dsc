0ef409fcaedad509c5c06444190cef197582a6be
eikek <eike.kettner@posteo.de>
2022-04-26 20:29:22 +0200

    Convert nix setup to flakes

--- default.nix
+++ default.nix
@@ -1,3 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.callPackage (import nix/release.nix) {}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).defaultNix
--- shell.nix
+++ shell.nix
@@ -1,27 +1,13 @@
-let
-  pkgs_source = import (builtins.fetchTarball "channel:nixos-21.11");
-  #pkgs = pkgs_source {};
-  moz_overlay = import (builtins.fetchTarball
-    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  pkgs = pkgs_source { overlays = [ moz_overlay ]; };
-  # arm = pkgs_source {
-  #   crossSystem = pkgs.lib.systems.examples.raspberryPi;
-  # };
-
-in
-pkgs.mkShell {
-  # nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
-  # buildInputs = with pkgs; [ rustfmt clippy ];
-
-  nativeBuildInputs = with pkgs;
-    [ pkg-config
-#      arm.pkg-config
-    ];
-  buildInputs = with pkgs;
-    [ pkgs.latest.rustChannels.stable.rust
-      pkgs.latest.rustChannels.stable.cargo
-      rustfmt
-    ];
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-#  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
